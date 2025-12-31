fd0560fa721267c380a139d480a4d08275b1d856
Teresa Binks <teresa@teresabinks.co.uk>
2025-12-31 12:30:47 +0000

    feat: introduce page request and string processing

--- src/plugins/burnsyweather/WeatherGetter.py
+++ src/plugins/burnsyweather/WeatherGetter.py
@@ -1,4 +1,14 @@
+from urllib.request import urlopen
+
 class WeatherGetter:
     def get_content(self):
      
-        return "It's going to be a superb day"
+        url = "http://olympus.realpython.org/profiles/aphrodite"
+        page = urlopen(url)
+        html_bytes = page.read()
+        html = html_bytes.decode("utf-8")
+        title_index = html.find("<title>")
+        start_index = title_index + len("<title>")
+        end_index = html.find("</title>")
+        title = html[start_index:end_index]
+        return f"It's going to be a superb day, {title}"

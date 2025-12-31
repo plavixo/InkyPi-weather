799141af6f8b1e4cdc5b3cfdccad2a32e7d21491
Teresa Binks <teresa@teresabinks.co.uk>
2025-12-31 12:23:30 +0000

    refactor: push code out to dedicated class

---
+++ src/plugins/burnsyweather/WeatherGetter.py
@@ -0,0 +1,4 @@
+class WeatherGetter:
+    def get_content(self):
+     
+        return "It's going to be a superb day"
--- src/plugins/burnsyweather/burnsyweather.py
+++ src/plugins/burnsyweather/burnsyweather.py
@@ -1,5 +1,6 @@
 from plugins.base_plugin.base_plugin import BasePlugin
 from utils.app_utils import resolve_path
+from plugins.burnsyweather.WeatherGetter import WeatherGetter
 
 # from PIL import Image, ImageDraw, ImageFont
 from utils.image_utils import resize_image
@@ -13,17 +14,9 @@ import os
 logger = logging.getLogger(__name__)
 
 class BurnsyWeather(BasePlugin):
-    # def generate_settings_template(self):
-    #     template_params = super().generate_settings_template()
-    #     template_params['api_key'] = {
-    #         "required": True,
-    #         "service": "OpenAI",
-    #         "expected_key": "OPEN_AI_SECRET"
-    #     }
-    #     template_params['style_settings'] = True
-    #     return template_params
 
     def generate_image(self, settings, device_config):
+        
         # api_key = device_config.load_env_key("OPEN_AI_SECRET")
         # if not api_key:
         #     raise RuntimeError("OPEN AI API Key not configured.")
@@ -32,28 +25,11 @@ class BurnsyWeather(BasePlugin):
         if not title:
             raise RuntimeError("Title is required.")
 
-        # text_model = settings.get('textModel')
-        # if not text_model:
-        #     raise RuntimeError("Text Model is required.")
-
-        # text_prompt = settings.get('textPrompt', '')
-        # if not text_prompt.strip():
-        #     raise RuntimeError("Text Prompt is required.")
-
-        # try:
-        #     ai_client = OpenAI(api_key = api_key)
-        #     prompt_response = AIText.fetch_text_prompt(ai_client, text_model, text_prompt)
-        # except Exception as e:
-        #     logger.error(f"Failed to make Open AI request: {str(e)}")
-        #     raise RuntimeError("Open AI request failure, please check logs.")
-
-        prompt_response ="Here's hoping it's a lovely day"
-
-
+        content = self.get_content()
 
         image_template_params = {
             "title": title,
-            "content": prompt_response,
+            "content": content,
             "plugin_settings": settings
         }
 
@@ -64,3 +40,18 @@ class BurnsyWeather(BasePlugin):
         image = self.render_image(dimensions, "burnsyweather.html", "burnsyweather.css", image_template_params)
 
         return image
+
+    def get_content(self):
+        weather_getter = WeatherGetter()
+        return weather_getter.get_content()
+        
+    
+    # def generate_settings_template(self):
+    #     template_params = super().generate_settings_template()
+    #     template_params['api_key'] = {
+    #         "required": True,
+    #         "service": "OpenAI",
+    #         "expected_key": "OPEN_AI_SECRET"
+    #     }
+    #     template_params['style_settings'] = True
+    #     return template_params

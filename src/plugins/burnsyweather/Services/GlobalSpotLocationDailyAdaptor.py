64fa5db2a51ae6bc78d8e5e5d7f28db61a690ceb
Teresa Binks <teresa@teresabinks.co.uk>
2026-01-01 15:46:55 +0000

    feat: begin processing daily data

--- src/plugins/burnsyweather/Services/GlobalSpotLocationDailyAdaptor.py
+++ src/plugins/burnsyweather/Services/GlobalSpotLocationDailyAdaptor.py
@@ -4,6 +4,12 @@ import os
 class GlobalSpotLocationDailyAdaptor:
     def get_spot_daily_forecast(self, weather_data, plugin_dir):     
         
-        global_spot_location_hours = {}
-          
-        return global_spot_location_hours
+        global_spot_location_daily = {}
+        
+        # for elements 2, 3, and 4 - tomowrrow, day after, and day after that
+        for i in range(2,5):
+            global_spot_location_daily[f"day_{i-1}_weather_symbol"] = os.path.join(plugin_dir, 'icons', 'old', f'{weather_data.features[0].properties.timeSeries[i].daySignificantWeatherCode}.svg')
+            global_spot_location_daily[f"day_{i-1}_temp_max"] = weather_data.features[0].properties.timeSeries[i].dayMaxScreenTemperature
+            global_spot_location_daily[f"day_{i-1}_temp_min"] = weather_data.features[0].properties.timeSeries[i].nightMinScreenTemperature
+     
+        return global_spot_location_daily

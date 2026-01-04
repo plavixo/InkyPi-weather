7dda3cc3fa80f319cc0b2b3249818ad0017d82a1
Teresa Binks <teresa@teresabinks.co.uk>
2026-01-04 15:44:15 +0000

    wip: separated concerns in orchestrator

--- src/plugins/burnsyweather/burnsyweather.py
+++ src/plugins/burnsyweather/burnsyweather.py
@@ -20,24 +20,41 @@ logger = logging.getLogger(__name__)
 class BurnsyWeather(BasePlugin):
 
     def generate_image(self, settings, device_config):
-        image_template_params = self.create_weather_tokens(settings)
 
-        dimensions = device_config.get_resolution()
-        if device_config.get_config("orientation") == "vertical":
-            dimensions = dimensions[::-1]
-        
-        image = self.render_image(dimensions, "burnsyweather.html", "burnsyweather.css", image_template_params)
-
-        return image    
-
-    def create_weather_tokens(self, settings):
+        # process settings
         lat = float(settings.get('latitude'))
         long = float(settings.get('longitude'))
         if not lat or not long:
             raise RuntimeError("Latitude and Longitude are required.")
         
-        icons_path = os.path.join(self.get_plugin_dir(), 'icons', 'old')
+        icon_set = "old"   # but could be put on settings html and fed through as settings.get('icon_set', 'old')
+
+        # Get the weather and ancillary data
+        weather_params = self.create_weather_tokens(lat, long, icon_set)
+
+        # Get device dimensions
+        dimensions = device_config.get_resolution()
+        if device_config.get_config("orientation") == "vertical":
+            dimensions = dimensions[::-1]
+
+        # Get Plugin params
+        basic_params = {
+            "title": 'MetOffice Weather',
+            "plugin_settings": settings,
+            "met_office_logo": self.get_plugin_dir('icons/Met_Office.png')
+        }
         
+        # Render the image
+        image_template_params = weather_params | basic_params
+        image = self.render_image(dimensions, "burnsyweather.html", "burnsyweather.css", image_template_params)
+
+        return image    
+
+    def create_weather_tokens(self, lat, long, icon_set):
+        
+        icons_path = os.path.join(self.get_plugin_dir(), 'icons', icon_set)
+        
+        # Get Weather and Location Data
         try: 
             global_spot_location_hours = GlobalSpotLocationHoursAdaptor().get_spot_hourly_forecast(icons_path, lat, long)
             global_spot_location_daily = GlobalSpotLocationDailyAdaptor().get_spot_daily_forecast(icons_path, lat, long)
@@ -48,13 +65,10 @@ class BurnsyWeather(BasePlugin):
             logger.error("Error getting weather content: %s", e)
             raise RuntimeError("Error retrieving weather data, please check logs.")
         
-        # Prepare Additional Params
-        standard_params = self.prepare_standard_params(settings)
 
         # Combine Params
         image_template_params = (
-            standard_params
-            | global_spot_location_hours
+            global_spot_location_hours
             | global_spot_location_daily
             | observation_params
             | location_params
@@ -63,13 +77,6 @@ class BurnsyWeather(BasePlugin):
         return image_template_params 
 
 
-    def prepare_standard_params(self, settings):
-        basic_params = {
-            "title": 'MetOffice Weather',
-            "plugin_settings": settings,
-            "met_office_logo": self.get_plugin_dir('icons/Met_Office.png')
-        }
-        return basic_params
     
     def get_city_from_coords(self, lat, lon):
         # Default to coordinate pair; attempt reverse geocode to get a friendly place name
